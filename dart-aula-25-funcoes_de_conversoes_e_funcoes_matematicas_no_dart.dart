8ba30b8a3b30b647efb66cd423d3de53e3f9aff2
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-19 13:35:31 -0300

    Aula 25 - Funções de Conversão e Funções Matemáticas no Dart

---
+++ dart-aula-25-funcoes_de_conversoes_e_funcoes_matematicas_no_dart.dart
@@ -0,0 +1,34 @@
+///
+/// Curso de Flutter e Dart
+/// Daves Balthazar
+/// Aula 25 - Funções de Conversão e Funções Matemáticas no Dart
+/// https://www.youtube.com/watch?v=-rpfXxoKI3I&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=27
+///
+void main() {
+  double pi = 3.1415;
+
+  print(pi);
+
+  /*
+  
+  if( pi.isNegative ) {
+    print( 'negativo' );
+  }
+  */
+
+  //print( pi.truncate() );
+
+  //print( pi.toStringAsFixed( 2 ) );
+
+  //print( pi.clamp( 1, 2 ) );
+
+  //print( 'O valor de PI é ' + pi.toString() );
+
+  //print( pi.ceil() );
+
+  //print( pi.floor() );
+
+  //print( pi.toInt() );
+
+  //print( pi.abs() );
+}
