327339082d4027a6f5ded6df0295c5453a50d370
Daves Balthazar <51927678+davesbalthazar@users.noreply.github.com>
2023-04-19 13:32:39 -0300

    Aula 20 - Const e Final em Dart

---
+++ dart-aula-20-const-e-final-em-dart.dart
@@ -0,0 +1,17 @@
+///
+/// Curso de Flutter e Dart
+/// Daves Balthazar
+/// Aula 20 - Const e Final em Dart
+/// https://www.youtube.com/watch?v=XxSl0bkTR54&list=PL5EmR7zuTn_Yu_YV2pT0h0843vRGiTMtx&index=23
+///
+void main() {
+  //const int a = 2;
+
+  //final int a = 2;
+
+  final DateTime data = new DateTime.now();
+
+  print(new DateTime.now());
+
+  //print( a );
+}
